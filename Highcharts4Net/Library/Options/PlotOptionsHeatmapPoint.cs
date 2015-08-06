25b33c1d9de8b50b8987c2535c4f0f13635ff4b9
David Castillo <davcs86@gmail.com>
2015-08-06 13:41:42 -0500

    PlotOptions.Heatmap added

---
+++ Highcharts4Net/Library/Options/PlotOptionsHeatmapPoint.cs
@@ -0,0 +1,12 @@
+﻿namespace Highcharts4Net.Library.Options
+{
+    /// <summary>
+    /// Properties for each single point
+    /// </summary>
+    public class PlotOptionsHeatmapPoint
+    {       /// <summary>
+            /// Events for each single point
+            /// </summary>
+        public PlotOptionsHeatmapPointEvents Events { get; set; }
+    }
+}
---
+++ Highcharts4Net/Library/Options/PlotOptionsHeatmapStates.cs
@@ -0,0 +1,13 @@
+﻿namespace Highcharts4Net.Library.Options
+{
+    /// <summary>
+	/// A wrapper object for all the series options in specific states.
+	/// </summary>
+    public class PlotOptionsHeatmapStates
+    {
+        /// <summary>
+		/// Options for the hovered series
+		/// </summary>
+		public PlotOptionsHeatmapStatesHover Hover { get; set; }
+    }
+}
